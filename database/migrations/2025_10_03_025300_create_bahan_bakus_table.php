fce42b35e0929c3e5681cf011c8fbd56465f0787
ums-l200220045 <l200220045@student.ums.ac.id>
2025-10-06 20:54:41 +0700

    first commit

---
+++ database/migrations/2025_10_03_025300_create_bahan_bakus_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bahan_bakus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_bahan')->unique();
+            $table->string('satuan');
+            $table->decimal('stok_terkini', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bahan_bakus');
+    }
+};
